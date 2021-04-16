11211fe35ca1ee147a45a3c4f8ae17bcf69797e6
mrifkikurniawan <mrifkikurniawan96@gmail.com>
2021-04-16 22:12:22 +0700

    modify activation arguments

--- lwr/utils/activation.py
+++ lwr/utils/activation.py
@@ -1,12 +1,10 @@
 from typing import Optional, Union
 
 from torch import nn
-from torch import functional as F
+from torch.nn import functional as F
 from torch import Tensor
 
 class SoftmaxT(nn.Module):
-    __constants__ = ['dim']
-    dim: Optional[int]
 
     def __init__(self, temperature: Union[float, int, Tensor]) -> None:
         super(SoftmaxT, self).__init__()
@@ -14,16 +12,13 @@ class SoftmaxT(nn.Module):
         if isinstance(temperature, float) or isinstance(temperature, int):
             temperature = Tensor([temperature])
         self.temperature = temperature
-    
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-        if not hasattr(self, 'dim'):
-            self.dim = None
 
-    def forward(self,   : Tensor, forward_softmax: bool) -> Tensor:
+    def forward(self, inputs: Tensor, act_f: str) -> Tensor:
         temperature = self.temperature.type_as(inputs)
         out = inputs/temperature
 
-        if forward_softmax:
-            out = F.softmax(out, dim=1, _stacklevel=5)
+        if act_f == 'softmax':
+            out = F.softmax(out, dim=1)
+        elif act_f == 'log_softmax':
+            out = F.log_softmax(out, dim=1)
         return out
