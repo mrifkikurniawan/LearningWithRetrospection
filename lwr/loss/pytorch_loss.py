66cd1baa724bfa583e839f8924b63ed83a5be661
mrifkikurniawan <mrifkikurniawan96@gmail.com>
2021-04-16 22:13:14 +0700

    create kldiv loss module

--- lwr/loss/pytorch_loss.py
+++ lwr/loss/pytorch_loss.py
@@ -1,4 +1,6 @@
-from torch import nn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 __all__ = ['CrossEntropyLoss', 'BCELoss', 'KLDivLoss']
 
@@ -8,5 +10,15 @@ def CrossEntropyLoss(**args):
 def BCELoss(**args):
     return nn.BCELoss(**args)
 
-def KLDivLoss(**args):
-    return nn.KLDivLoss(**args)
+class KLDivLoss(nn.Module):
+    def __init__(self, temperature: float, **kwargs):
+        super(KLDivLoss, self).__init__()
+        self.temperature = torch.Tensor([temperature])
+        
+    def forward(self, logits, y):
+        y = y.type_as(logits)
+        self.temperature = self.temperature.type_as(logits)
+        
+        y_hat = F.log_softmax(logits/self.temperature, dim=1)
+        loss = F.kl_div(y_hat, y, reduction='batchmean') * (self.temperature**2)
+        return loss
